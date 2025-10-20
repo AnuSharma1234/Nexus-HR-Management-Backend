8398b710336f947241a8601fd2a4e8d34131ac8a
anurag <anuragsharma@gmail.com>
2025-10-20 12:32:18 +0530

    postgres connection hussssh

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,20 @@
 import { config } from 'dotenv'
 config()
 
+//server backup port
 const DEFAULT_PORT = 3000
 
+//default port
 export const PORT = process.env.PORT ? Number(process.env.PORT) : DEFAULT_PORT
 
+
+//winston logger active log level
 export const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
 
 
-
+//mongo db uri
 export const MONGO_URI = process.env.MONGO_URI ? String(process.env.MONGO_URI) : ''
+
+//postgres db configs
+
 
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import { PORT } from "./config";
 import app from "./app";
 import logger from "./utils/logger.util";
 import connectMongoDb from "./database/mongodb/connect.db";
+import prisma from "./database/postgres/prisma.client";
 
 app.listen(PORT, async () => {
     try{
         await connectMongoDb()
+        await prisma.$connect()
+        logger.info(`Connected to PostgreSQL Succesfully`)
         logger.info(`Server started at http://localhost:${PORT}`)
     }catch(error){
         logger.error(error)
